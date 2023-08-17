7d1d06e80b3dc6fa94959a7cbf0366c073f3cbbc
eveeifyeve <eveeg1971@gmail.com>
2023-08-17 15:49:37 +1000

    made a discord bot.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
-mod handler;
-mod commands;
-mod events;
+use anyhow::anyhow;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use shuttle_secrets::SecretStore;
+use tracing::{error, info};
 
+struct Bot;
 
-
-use handler::Handler;
-
-#[tokio::main]
-async fn main() {
-    let token = "your bot token here";
-
-    let application_id: u64 = "your application id here".parse().expect("application id is not a valid id");
-
-    let mut client = Client::builder(token, GatewayIntents::GUILD_MESSAGES | GatewayIntents::GUILD_MESSAGE_REACTIONS )
-        .event_handler(Handler)
-        .application_id(application_id)
-        .await
-        .expect("Error creating client");
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
+#[async_trait]
+impl EventHandler for Bot {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!botinfo" {
+            if let Err(e) = msg.channel_id.say(&ctx.http, "This bot is made by eveeify LICENCED UNDER MIT").await {
+                error!("Error sending message: {:?}", e);
+            }
+        }
     }
+
+    async fn ready(&self, _: Context, ready: Ready, secret_store: SecretStore) {
+        info!("{} is connected!", ready.user.name);
+
+        GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands.create_application_command(|command| { command.name("botinfo").description("gives you the bot info") })
+        }).await.unwrap();
+
+    }
+}
+
+async fn interactionCreate()
+
+#[shuttle_runtime::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
+        token
+    } else {
+        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
+    };
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .await
+        .expect("Err creating client");
+
+    Ok(client.into())
 }
